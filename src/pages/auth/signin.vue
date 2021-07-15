4ef49455381f4e61d0536ce3ac39caa7e2db099f
Ejaaz Khan <ejaazrkhan@gmail.com>
2021-07-15 23:16:34 +0530

    Created Signin and SignUp Form

---
+++ src/pages/auth/signin.vue
@@ -0,0 +1,53 @@
+<template>
+  <f7-page name="signin">
+    <f7-navbar title="Sign In" back-link="Back"></f7-navbar>
+
+    <f7-list no-hairlines-md>
+
+        <f7-list-input
+            :value="email"
+            @input="email=$event.target.value"
+            label="E-mail"
+            type="email"
+            placeholder="Your e-mail"
+            clear-button
+        >
+            <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
+        </f7-list-input>
+
+        <f7-list-input
+            :value="password"
+            @input="password=$event.target.value"
+            label="Password"
+            type="password"
+            placeholder="Your password"
+            clear-button
+        >
+            <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
+        </f7-list-input>
+    </f7-list>
+
+    <f7-block>
+
+        <f7-button outline>Sign In</f7-button>
+
+        <div style="text-align: center;">
+            <f7-link>Resend Confirmation Email</f7-link><br />
+            <f7-link href="/signup/">Don't Have Account? Sign Up</f7-link><br />
+            <f7-link>Forget Password</f7-link>
+        </div>
+
+    </f7-block>
+
+  </f7-page>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            email:null,
+            password:null
+        }
+    }
+}
+</script>
---
+++ src/pages/auth/signup.vue
@@ -0,0 +1,64 @@
+<template>
+  <f7-page name="signup">
+    <f7-navbar title="Sign Up" back-link="Back"></f7-navbar>
+
+    <div class="wrapper">
+        <img data-src="https://www.dgvaishnavcollege.edu.in/dgvaishnav-c/uploads/2021/01/dummy-profile-pic.jpg" alt="" class="lazy">
+    </div>
+
+    <f7-list no-hairlines-md>
+
+        <f7-list-input
+            :value="name"
+            @input="name=$event.target.value"
+            label="Name"
+            type="text"
+            placeholder="Your name"
+            clear-button
+        >
+            <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
+        </f7-list-input>
+
+        <f7-list-input
+            :value="email"
+            @input="email=$event.target.value"
+            label="E-mail"
+            type="email"
+            placeholder="Your e-mail"
+            clear-button
+        >
+            <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
+        </f7-list-input>
+
+        <f7-list-input
+            :value="password"
+            @input="password=$event.target.value"
+            label="Password"
+            type="password"
+            placeholder="Your password"
+            clear-button
+        >
+            <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
+        </f7-list-input>
+    </f7-list>
+
+    <f7-block>
+
+        <f7-button outline>Sign In</f7-button>
+
+    </f7-block>
+
+  </f7-page>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            name:null,
+            email:null,
+            password:null,
+            image_url:"https://www.dgvaishnavcollege.edu.in/dgvaishnav-c/uploads/2021/01/dummy-profile-pic.jpg"
+        }
+    }
+}
+</script>
